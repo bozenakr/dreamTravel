bf642b81e0a955f21b265882c30b97415b9806d9
bozenakr <bozena.krasevic@gmail.com>
2023-02-06 08:10:55 +0200

    initial commit

---
+++ database/migrations/2023_02_06_054545_create_hotels_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('hotels', function (Blueprint $table) {
+            $table->id();
+            $table->string('title', 100);
+            $table->decimal('price', 10, 2)->unsigned();
+            $table->integer('time')->unsigned();
+            $table->unsignedBigInteger('country_id');
+            $table->foreign('country_id')->references('id')->on('countries');
+            $table->string('photo', 200)->nullable();
+            $table->text('desc')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('hotels');
+    }
+};
